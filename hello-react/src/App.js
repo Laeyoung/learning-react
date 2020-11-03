b5efdb40b96ea7164ca33aee9b8e7bcf42e75dc8
Laeyoung <gadise@gmail.com>
2020-11-03 07:28:43 +0000

    add hello-react project

---
+++ hello-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>야야야야</h1>
+      <h2>TTA 하셔야해요!!</h2>
+    </Fragment>
+  );
+}
+
+export default App;
